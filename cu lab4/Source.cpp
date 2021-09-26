2eac6ded456461a73a0bc0386d2e405c2a23a0b9
HYPERPC <HYPERPC@DESKTOP-9FMUOVG>
2021-09-26 17:30:26 +0300

    Добавьте файлы проекта.

---
+++ cu lab4/Source.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+	float a, b, S, P;
+	a = 3;
+	printf("a = ");
+	printf("%f\n", a);
+	b = 2;
+	printf("b = ");
+	printf("%f\n", b);
+	S = a * b;
+	printf("S = ");
+	printf("%f\n", S);
+	P = 2 * (a + b);
+	printf("P = ");
+	printf("%f\n", P);
+}
